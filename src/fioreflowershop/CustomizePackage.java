f836a758fcd9f34af11ea3c05769f6566280d622
Tesla <chiupeeng@gmail.com>
2018-11-06 17:46:54 +0800

    Initial Commit

---
+++ src/fioreflowershop/CustomizePackage.java
@@ -0,0 +1,157 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fioreflowershop;
+import fioreflowershop.modal.CustomizedPackage;
+import fioreflowershop.modal.Item;
+import java.util.ArrayList;
+import java.util.Scanner;
+/**
+ *
+ * @author Chiu Peeng
+ */
+  
+public class CustomizePackage {
+    private static ArrayList<Item> styles = new ArrayList<>();
+    private static ArrayList<Item> sizes = new ArrayList<>();
+    private static ArrayList<Item> flowers = new ArrayList<>();
+    private static ArrayList<Item> accessories = new ArrayList<>();
+    private static ArrayList<Item> priorities = new ArrayList<>();
+    
+    public static void CustomizePackageControl(){
+        int style = 0, size = 0, flower = 0, accessory = 0, priority = 0;
+        boolean exception;
+        Scanner scan = new Scanner(System.in);
+        
+        InitializePackages();
+        
+        do{
+            try{
+                exception = false;
+                StartUI(1);
+                style = scan.nextInt() - 1;
+            }catch(Exception e){
+                exception = true;
+            }
+        }while(exception == true);
+        
+        do{
+            try{
+                exception = false;
+                StartUI(2);
+                size = scan.nextInt() - 1;
+            }catch(Exception e){
+                exception = true;
+            }
+        }while(exception == true);
+        
+        do{
+            try{
+                exception = false;
+                StartUI(3);
+                flower = scan.nextInt() - 1;
+            }catch(Exception e){
+                exception = true;
+            }
+        }while(exception == true);
+        
+        do{
+            try{
+                exception = false;
+                StartUI(4);
+                accessory = scan.nextInt() - 1;
+            }catch(Exception e){
+                exception = true;
+            }
+        }while(exception == true);
+        
+        do{
+            try{
+                exception = false;
+                StartUI(5);
+                priority = scan.nextInt() - 1;
+            }catch(Exception e){
+                exception = true;
+            }
+        }while(exception == true);
+        
+        CustomizedPackage order = new CustomizedPackage(styles.get(style), sizes.get(size), flowers.get(flower), accessories.get(accessory), priorities.get(priority));
+        double price = order.CalculateOrder();
+        System.out.println(price);
+    }
+    
+    public static void StartUI(int step) throws Exception{
+        int i = 0;
+        if(step == 1){
+            System.out.println("Select the flower arrangement style");
+            for(Item item:styles){
+                ++i;
+                System.out.printf("[%d] %s: RM%.2f\n", i, item.getName(), item.getPrice());
+            }
+        }
+        else if(step == 2){
+            System.out.println("Select the floral arrangement size");
+            System.out.println("This will be multiplied by the selected flower's price");
+            for(Item item:sizes){
+                ++i;
+                System.out.printf("[%d] %s: Flower Price x %.0f\n", i, item.getName(), item.getPrice());
+            }
+        }
+        else if(step == 3){
+            System.out.println("Select the flowers for the arrangement");
+            for(Item item:flowers){
+                ++i;
+                System.out.printf("[%d] %s: RM%.2f\n", i, item.getName(), item.getPrice());
+            }
+        }
+        else if(step == 4){
+            System.out.println("Select the accessory to be added");
+            for(Item item:accessories){
+                ++i;
+                System.out.printf("[%d] %s: RM%.2f\n", i, item.getName(), item.getPrice());
+            }
+        }
+        else if(step == 5){
+            System.out.println("Select the delivery priority");
+            System.out.println("This will be multiplied by the sum of the floral arrangement");
+            for(Item item:priorities){
+                ++i;
+                System.out.printf("[%d] %s: Order price x %.0f\n", i, item.getName(), item.getPrice());
+            }
+        }
+        else{
+            System.out.println("Oops, something went wrong");
+            throw new Exception();
+        }
+    }
+    
+    public static void InitializePackages(){
+       styles.add(new Item("Fan", 10));
+       styles.add(new Item("Elliptical", 10));
+       styles.add(new Item("Vertical", 10));
+       styles.add(new Item("Horizontal", 10));
+       styles.add(new Item("Triangular", 10));
+       
+       sizes.add(new Item("Small", 1));
+       sizes.add(new Item("Medium", 2));
+       sizes.add(new Item("Large", 4));
+       
+       flowers.add(new Item("Sunflowers", 250));
+       flowers.add(new Item("Red Roses", 300));
+       flowers.add(new Item("White Roses", 250));
+       flowers.add(new Item("Tulips", 450));
+       flowers.add(new Item("Daffodils", 200));
+       
+       accessories.add(new Item("None", 0));
+       accessories.add(new Item("Name Card", 5));
+       accessories.add(new Item("Bears", 50));
+       accessories.add(new Item("Woven Basket", 35));
+       
+       priorities.add(new Item("Flexi", 1));
+       priorities.add(new Item("Normal", 5));
+       priorities.add(new Item("Express", 10));
+    }
+    
+}
---
+++ src/fioreflowershop/modal/CustomizedPackage.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fioreflowershop.modal;
+
+/**
+ *
+ * @author Chiu Peeng
+ */
+public class CustomizedPackage {
+    private static int orderNo = 1;
+    private Item style, size, flower, accessory, priority;
+
+    public static int getOrderNo() {
+        return orderNo;
+    }
+
+    public static void setOrderNo(int orderNo) {
+        CustomizedPackage.orderNo = orderNo;
+    }
+
+    public Item getStyle() {
+        return style;
+    }
+
+    public void setStyle(Item style) {
+        this.style = style;
+    }
+
+    public Item getSize() {
+        return size;
+    }
+
+    public void setSize(Item size) {
+        this.size = size;
+    }
+
+    public Item getFlower() {
+        return flower;
+    }
+
+    public void setFlower(Item flower) {
+        this.flower = flower;
+    }
+
+    public Item getAccessory() {
+        return accessory;
+    }
+
+    public void setAccessory(Item accessory) {
+        this.accessory = accessory;
+    }
+
+    public Item getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Item priority) {
+        this.priority = priority;
+    }
+
+    public CustomizedPackage(Item style, Item size, Item flower, Item accessory, Item priority) {
+        this.style = style;
+        this.size = size;
+        this.flower = flower;
+        this.accessory = accessory;
+        this.priority = priority;
+    }
+
+    public CustomizedPackage() {
+    }
+    
+    public double CalculateOrder(){
+        return (style.getPrice() + (flower.getPrice() * size.getPrice()) + accessory.getPrice()) * priority.getPrice();
+    }
+}
