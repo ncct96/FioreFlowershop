cadec21158da4723a68cf26c27f56534fd30135b
ncct96 <30764188+ncct96@users.noreply.github.com>
2018-11-10 06:40:59 +0800

    Updated ArrayList ADT

---
+++ src/fioreflowershop/Pickup.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fioreflowershop;
+
+import fioreflowershop.adt.ArrayList;
+import fioreflowershop.adt.ListInterface;
+import fioreflowershop.modal.CustomizedPackage;
+import fioreflowershop.modal.Item;
+
+/**
+ *
+ * @author Nicholas
+ */
+public class Pickup {
+    
+    ListInterface<CustomizedPackage> customPackageList = new ArrayList<>();
+    
+//    for(int i = 0; i < customPack ; i++ ){
+//    
+//}
+//    
+    public void initOrderList(){
+
+    }
+}
