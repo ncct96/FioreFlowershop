6fb74bd8a5947a9d1b53beb090a12b94bbd4ca03
ncct96 <30764188+ncct96@users.noreply.github.com>
2018-11-11 07:24:59 +0800

    Updated Pickup Order Sorting

--- src/fioreflowershop/modal/Order.java
+++ src/fioreflowershop/modal/Order.java
@@ -12,5 +12,48 @@ package fioreflowershop.modal;
 public class Order {
     private int orderID;
     private String orderType;
+    private CorporateCustomer corp;
+    private CustomizedPackage customPackage;
+
+    public Order(int orderID, String orderType, CorporateCustomer corp, CustomizedPackage customPackage) {
+        this.orderID = orderID;
+        this.orderType = orderType;
+        this.corp = corp;
+        this.customPackage = customPackage;
+    }
+
+    public int getOrderID() {
+        return orderID;
+    }
+
+    public String getOrderType() {
+        return orderType;
+    }
+
+    public CorporateCustomer getCorp() {
+        return corp;
+    }
+
+    public CustomizedPackage getCustomPackage() {
+        return customPackage;
+    }
+
+    public void setOrderID(int orderID) {
+        this.orderID = orderID;
+    }
+
+    public void setOrderType(String orderType) {
+        this.orderType = orderType;
+    }
+
+    public void setCorp(CorporateCustomer corp) {
+        this.corp = corp;
+    }
+
+    public void setCustomPackage(CustomizedPackage customPackage) {
+        this.customPackage = customPackage;
+    }
+    
+    
     
 }
