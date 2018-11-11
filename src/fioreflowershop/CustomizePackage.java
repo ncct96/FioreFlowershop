eb5acb6eed772341ee90994d53f0b997382e35de
Tesla <chiupeeng@gmail.com>
2018-11-11 12:42:48 +0800

    Added Customer attribute

--- src/fioreflowershop/CustomizePackage.java
+++ src/fioreflowershop/CustomizePackage.java
@@ -9,7 +9,7 @@ import fioreflowershop.modal.CustomizedPackage;
 import fioreflowershop.modal.Item;
 import fioreflowershop.adt.ArrayList;
 import fioreflowershop.adt.ListInterface;
-//import java.util.ArrayList;
+import fioreflowershop.modal.Customer;
 import java.util.Scanner;
 
 /**
@@ -27,7 +27,7 @@ public class CustomizePackage {
 
     public static ListInterface<CustomizedPackage> customPackageList = new ArrayList<>();
 
-    public static void CustomizePackageControl() {
+    public static void CustomizePackageControl(Customer customer) {
         int style = 0, size = 0, flower = 0, accessory = 0, priority = 0, deliveryType = 0, loop = 0;
         boolean exception;
         Scanner scan = new Scanner(System.in);
@@ -113,7 +113,7 @@ public class CustomizePackage {
             }
         } while (exception == true);
 
-        CustomizedPackage order = new CustomizedPackage(styles.getItem(style), sizes.getItem(size), flowers.getItem(flower), accessories.getItem(accessory), priorities.getItem(priority), deliveryTypes.getItem(deliveryType));
+        CustomizedPackage order = new CustomizedPackage(styles.getItem(style), sizes.getItem(size), flowers.getItem(flower), accessories.getItem(accessory), priorities.getItem(priority), deliveryTypes.getItem(deliveryType), customer);
         customPackageList.add(order);
         double price = order.CalculateOrder();
         order.minusQuantity();
@@ -140,61 +140,33 @@ public class CustomizePackage {
             for (int i = 1; i <= styles.getTotalEntries(); i++) {
                 System.out.printf("[%d] %s: RM%.2f\n", i, styles.getItem(i).getName(), styles.getItem(i).getPrice());
             }
-//            for(Item item:styles){
-//                ++i;
-//                System.out.printf("[%d] %s: RM%.2f\n", i, item.getName(), item.getPrice());
-//            }
         } else if (step == 2) {
             System.out.println("Select the floral arrangement size");
             System.out.println("This will be multiplied by the selected flower's price");
             for (int i = 1; i <= sizes.getTotalEntries(); i++) {
                 System.out.printf("[%d] %s: Flower Price x %.0f\n", i, sizes.getItem(i).getName(), sizes.getItem(i).getPrice());
             }
-//            for(Item item:sizes){
-//                ++i;
-//                System.out.printf("[%d] %s: Flower Price x %.0f\n", i, item.getName(), item.getPrice());
-//            }
         } else if (step == 3) {
             System.out.println("Select the flowers for the arrangement");
             for (int i = 1; i <= flowers.getTotalEntries(); i++) {
                 System.out.printf("[%d] %s: RM%.2f\n", i, flowers.getItem(i).getName(), flowers.getItem(i).getPrice());
             }
-//            for(Item item:flowers){
-//                if(item.getQuantity() > 0){
-//                    ++i;
-//                    System.out.printf("[%d] %s: RM%.2f\n", i, item.getName(), item.getPrice());
-//                }
-//            }
         } else if (step == 4) {
             System.out.println("Select the accessory to be added");
             for (int i = 1; i <= accessories.getTotalEntries(); i++) {
                 System.out.printf("[%d] %s: RM%.2f\n", i, accessories.getItem(i).getName(), accessories.getItem(i).getPrice());
             }
-//            for(Item item:accessories){
-//                if(item.getQuantity() > 0){
-//                    ++i;
-//                    System.out.printf("[%d] %s: RM%.2f\n", i, item.getName(), item.getPrice());
-//                }
-//            }
         } else if (step == 5) {
             System.out.println("Select the delivery priority");
             System.out.println("This will be multiplied by the sum of the floral arrangement");
             for (int i = 1; i <= priorities.getTotalEntries(); i++) {
                 System.out.printf("[%d] %s: Order price x %.0f\n", i, priorities.getItem(i).getName(), priorities.getItem(i).getPrice());
             }
-//            for(Item item:priorities){
-//                ++i;
-//                System.out.printf("[%d] %s: Order price x %.0f\n", i, item.getName(), item.getPrice());
-//            }
         } else if (step == 6) {
             System.out.println("Select the delivery type");
             for (int i = 1; i <= deliveryTypes.getTotalEntries(); i++) {
                 System.out.printf("[%d] %s: Extra Charges: RM%.0f\n", i, deliveryTypes.getItem(i).getName(), deliveryTypes.getItem(i).getPrice());
             }
-//            for(Item item:priorities){
-//                ++i;
-//                System.out.printf("[%d] %s: Extra Charges: RM%.0f\n", i, item.getName(), item.getPrice());
-//            }
         } else if (step == 7) {
             System.out.println("DDD");
             Pickup pickup = new Pickup();
