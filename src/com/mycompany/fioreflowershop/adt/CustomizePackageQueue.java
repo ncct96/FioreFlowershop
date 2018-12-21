bfa3a090b9206aad0e310c55eff75cc51164d7ce
Tesla <chiupeeng@gmail.com>
2018-12-21 16:54:11 +0800

    Convert Queue to link

--- src/com/mycompany/fioreflowershop/adt/CustomizePackageQueue.java
+++ src/com/mycompany/fioreflowershop/adt/CustomizePackageQueue.java
@@ -7,98 +7,85 @@ package com.mycompany.fioreflowershop.adt;
 
 /**
  *
- * @author Admin
+ * @author Chiu Peeng
  */
 public class CustomizePackageQueue<T> implements CustomizePackageQueueInterface<T> {
 
-  private T[] queue;
-  private final static int frontIndex = 0; //Front of the queue is fixed, since first index is 0
-  private int backIndex;
-  private static final int DEFAULT_INITIAL_CAPACITY = 50;
+    private Node firstNode;
+    private Node lastNode;
+    private int size = 0;
 
-  public CustomizePackageQueue() {
-    this(DEFAULT_INITIAL_CAPACITY);
-  } // end default constructor
-
-  public CustomizePackageQueue(int initialCapacity) {
-    queue = (T[]) new Object[initialCapacity];
-    backIndex = -1;//intialized to -1 to indicate an empty queue
-  } // end constructor
-
-  //Adds a new entry to the back of the queue.
-  public void addCustomizedPackage(T newEntry) {
-    if (isArrayFull()) // isArrayFull and
-    {
-      doubleArray();   // doubleArray are private
-    }
-    backIndex++;
-    queue[backIndex] = newEntry;
-  } // end enqueue
-
-  //Retrieves the entry at the front of the queue.
-  public T getFirstPackage() {
-    T front = null;
-
-    if (!isEmpty()) {
-      front = queue[frontIndex];
+    public CustomizePackageQueue() {
+        firstNode = null;
+        lastNode = null;
     }
 
-    return front;
-  } // end getFront
+    public void addCustomizedPackage(T newEntry) {
+        Node newNode = new Node(newEntry, null);
 
+        if (isEmpty()) {
+            firstNode = newNode;
+        } else {
+            lastNode.next = newNode;
+        }
 
-  //Removes and returns the entry at the front of the queue.
-  public T removeCustomizedPackage() {
-    T front = null;
-
-    if (!isEmpty()) {
-      front = queue[frontIndex];
-
-      // shift remaining queue items forward one position
-      for (int i = frontIndex; i < backIndex; ++i) {
-        queue[i] = queue[i + 1];
-      }
-      queue[backIndex] = null;
-      backIndex--;
-    } // end if
-
-    return front;
-  } // end dequeue
-
-  //Detects whether .the queue is empty.
-  public boolean isEmpty() {
-    return frontIndex > backIndex;
-  } // end isEmpty
-
-  //Removes all entries from the queue.
-  public void clear() {
-    if (!isEmpty()) { // deallocates only the used portion
-      for (int index = frontIndex; index <= backIndex; index++) {
-        queue[index] = null;
-      } // end for
-
-      backIndex = -1;
-    } // end if
-  } // end clear
-
-  //Detects whether the queue is full
-  private boolean isArrayFull() {
-    return backIndex == queue.length - 1;
-  } // end isArrayFull
-
-    public int getBackIndex() {
-        return backIndex;
+        lastNode = newNode;
+        ++size;
     }
 
-  //Doubles the size of the old Queue list
-  private void doubleArray() {
-    T[] oldQueue = queue;
-    int oldSize = oldQueue.length;
+    public T getFirstPackage() {
+        T front = null;
 
-    queue = (T[]) new Object[2 * oldSize];
+        if (!isEmpty()) {
+            front = firstNode.data;
+        }
 
-    for (int index = 0; index < oldSize; index++) {
-      queue[index] = oldQueue[index];
-    } // end for
-  } // end doubleArray
-}  // end ArrayQueue
+        return front;
+    }
+
+    public T removeCustomizedPackage() {
+        T front = null;
+
+        if (!isEmpty()) {
+            front = firstNode.data;
+            firstNode = firstNode.next;
+
+            if (firstNode == null) {
+                lastNode = null;
+            }
+
+            --size;
+        }
+        return front;
+    }
+
+    public boolean isEmpty() {
+        return (firstNode == null) && (lastNode == null);
+    }
+
+    public void clear() {
+        firstNode = null;
+        lastNode = null;
+        size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    private class Node {
+
+        private T data;
+        private Node next;
+
+        private Node(T dataPortion) {
+            data = dataPortion;
+            next = null;
+        }
+
+        private Node(T dataPortion, Node linkPortion) {
+            data = dataPortion;
+            next = linkPortion;
+        }
+    }
+}
--- src/com/mycompany/fioreflowershop/adt/CustomizePackageQueueInterface.java
+++ src/com/mycompany/fioreflowershop/adt/CustomizePackageQueueInterface.java
@@ -10,40 +10,16 @@ package com.mycompany.fioreflowershop.adt;
  * @author Admin
  */
 public interface CustomizePackageQueueInterface<T> {
-    /**
-   * Task: Adds a new entry to the back of the queue.
-   *
-   * @param newEntry an object to be added
-   */
-  public void addCustomizedPackage(T newEntry);
 
-  /**
-   * Task: Removes and returns the entry at the front of the queue.
-   *
-   * @return either the object at the front of the queue or, if the queue is
-   * empty before the operation, null
-   */
-  public T removeCustomizedPackage();
+    public void addCustomizedPackage(T newEntry);
 
-  /**
-   * Task: Retrieves the entry at the front of the queue.
-   *
-   * @return either the object at the front of the queue or, if the queue is
-   * empty, null
-   */
-  public T getFirstPackage();
+    public T removeCustomizedPackage();
 
-  /**
-   * Task: Detects whether .the queue is empty.
-   *
-   * @return true if the queue is empty, or false otherwise
-   */
-  public boolean isEmpty();
+    public T getFirstPackage();
 
-  /**
-   * Task: Removes all entries from the queue.
-   */
-  public void clear();
- 
-  public int getBackIndex();
+    public boolean isEmpty();
+
+    public void clear();
+
+    public int getSize();
 }
